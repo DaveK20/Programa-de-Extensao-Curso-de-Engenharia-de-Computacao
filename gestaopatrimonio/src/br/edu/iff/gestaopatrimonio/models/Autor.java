73d3bb2ff2be19c2b937b8fc0c32695b72665c67
arlisson <arlisson234529@gmail.com>
2023-07-16 12:05:03 -0300

    Classe autor OK

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Autor.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Autor.java
@@ -12,4 +12,52 @@ public class Autor {
 
     public List<NomeAlternativo> nomeAlternativo = new ArrayList<NomeAlternativo> ();
 
+	
+
+	public Autor(int id, String nome, String biografia, List<NomeAlternativo> nomeAlternativo) {
+		super();
+		setId(id);
+		setNome(nome);
+		setBiografia(biografia);
+		setNomeAlternativo(nomeAlternativo);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getBiografia() {
+		return biografia;
+	}
+
+	public void setBiografia(String biografia) {
+		this.biografia = biografia;
+	}
+
+	public List<NomeAlternativo> getNomeAlternativo() {
+		return nomeAlternativo;
+	}
+
+	public void setNomeAlternativo(List<NomeAlternativo> nomeAlternativo) {
+		this.nomeAlternativo = nomeAlternativo;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [id=" + id + ", nome=" + nome + ", biografia=" + biografia + ", nomeAlternativo="
+				+ nomeAlternativo + "]";
+	}
+
 }
