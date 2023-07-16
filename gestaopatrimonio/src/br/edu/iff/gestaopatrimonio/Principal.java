44ee53bb0731205907c2c38063e6375ab4bc21b7
arlisson <arlisson234529@gmail.com>
2023-07-16 17:21:07 -0300

    Principal testes

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/Principal.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/Principal.java
@@ -1,10 +1,16 @@
 package br.edu.iff.gestaopatrimonio;
 
+import br.edu.iff.gestaopatrimonio.daos.*;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		AutorDAO autordao = new AutorDAO();
+		
+		
+		
 	}
 
 }
