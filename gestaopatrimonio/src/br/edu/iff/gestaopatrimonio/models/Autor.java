f56f52c806a9a4de90f304a1a7079e16667a79ce
arlisson <arlisson234529@gmail.com>
2023-07-16 19:15:06 -0300

    Autor.Java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Autor.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Autor.java
@@ -57,8 +57,8 @@ public class Autor {
 
 	@Override
 	public String toString() {
-		return "Autor [id=" + id + ", nome=" + nome + ", biografia=" + biografia + ", nomeAlternativo="
-				+ nomeAlternativo + "]";
+		return "Autor [id= " + id + ", nome= " + nome + ", biografia= " + biografia + ", nomeAlternativo= "
+				+ nomeAlternativo + "]\n";
 	}
 
 }
