fa491bb6386aad0e30dbbddd67b4accee6f2a8f5
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-07-17 00:08:21 -0300

    Update NomeAlternativoController.java
    Corrigi Parte do Código do João com autorização dele

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/NomeAlternativoController.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/NomeAlternativoController.java
@@ -1,5 +1,8 @@
-package br.edu.iff.gestaopatrimonio.daos;
+package br.edu.iff.gestaopatrimonio.controllers;
 import java.util.List;
+
+import br.edu.iff.gestaopatrimonio.daos.NomeAlternativoDAO;
+import br.edu.iff.gestaopatrimonio.models.NomeAlternativo;
 
 public class NomeAlternativoController {
     private NomeAlternativoDAO nomeAlternativoDAO;
