4c0ebf895cefe2c4dba1508cb784bc988b71b0af
Ayrton Borges <42078126+AyrtonBorges@users.noreply.github.com>
2023-07-16 21:49:49 -0300

    Add files via upload

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Aquisicao.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Aquisicao.java
@@ -6,5 +6,35 @@ public class Aquisicao {
     private String data;
 
     private FormaAquisicao formaAquisicao;
+    
+    public Aquisicao(String data, FormaAquisicao formaAquisicao)
+    {
+    	this.data = data;
+    	this.formaAquisicao = formaAquisicao;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public FormaAquisicao getFormaAquisicao() {
+		return formaAquisicao;
+	}
+
+	public void setFormaAquisicao(FormaAquisicao formaAquisicao) {
+		this.formaAquisicao = formaAquisicao;
+	}
 
 }
