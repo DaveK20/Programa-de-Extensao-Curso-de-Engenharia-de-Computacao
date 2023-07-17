9d0a4ad4a796717801b684dd30db06dc8a3bac27
PedroeRB <42224724+PedroeRB@users.noreply.github.com>
2023-07-16 22:36:53 -0300

    Add files via upload

---
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/NomeAlternativoController.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class NomeAlternativoController {
+    private NomeAlternativoDAO nomeAlternativoDAO;
+
+    public NomeAlternativoController() {
+        this.nomeAlternativoDAO = new NomeAlternativoDAO();
+    }
+
+    public List<NomeAlternativo> listarNomesAlternativos() {
+        return nomeAlternativoDAO.listarNomesAlternativos();
+    }
+
+    public void adicionarNomeAlternativo(NomeAlternativo nomeAlternativo) {
+        nomeAlternativoDAO.adicionarNomeAlternativo(nomeAlternativo);
+    }
+
+    public void atualizarNomeAlternativo(NomeAlternativo nomeAlternativo) {
+        nomeAlternativoDAO.atualizarNomeAlternativo(nomeAlternativo);
+    }
+
+    public void removerNomeAlternativo(int id) {
+        nomeAlternativoDAO.removerNomeAlternativo(id);
+    }
+}
