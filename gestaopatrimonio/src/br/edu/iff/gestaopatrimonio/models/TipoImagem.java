3b8f87fc9b502dc8e7042387be896103f0f6e07c
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-07-17 01:24:38 -0300

    Update TipoImagem.java
    Finalizei a classe modelo do tipo de imagem

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/TipoImagem.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/TipoImagem.java
@@ -3,10 +3,52 @@ package br.edu.iff.gestaopatrimonio.models;
 public class TipoImagem {
 	private int id;
 
-	private String largura;
+	private int largura;
 
-	private String altura;
+	private int altura;
 
 	private String descricao;
+
+	public TipoImagem() {
+	}
+
+	public TipoImagem(int id, int largura, int altura, String descricao) {
+		setId(id);
+		setAltura(altura);
+		setLargura(largura);
+		setDescricao(descricao);
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getLargura() {
+		return largura;
+	}
+
+	public void setLargura(int largura) {
+		this.largura = largura;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
 
 }
