1060a47e4b0308e9595d63b24b12e9c155a5c58b
lucaspicancof <116851208+lucaspicancof@users.noreply.github.com>
2023-07-20 15:45:22 -0300

    Update TipoPatrimonioController.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/TipoPatrimonioController.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/TipoPatrimonioController.java
@@ -1,1 +1,52 @@
+package br.edu.iff.gestaopatrimonio.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.iff.gestaopatrimonio.daos.TipoPatrimonioDAO;
+import br.edu.iff.gestaopatrimonio.models.TipoPatrimonio;
+
+public class TipoPatrimonioController {
+	TipoPatrimonioDAO tipo_patrimonioDAO = new TipoPatrimonioDAO();
+	
+	public TipoPatrimonio cadastrarTipoPatrimonio(String nome_tipo_patrimonio) {
+		
+		if(nome_tipo_patrimonio.length()<=45) {
+			TipoPatrimonio tipo_patrimonio = tipo_patrimonioDAO.cadastrarTipoPatrimonio(nome_tipo_patrimonio);
+			return tipo_patrimonio;
+		}
+		return null;
+	}
+	
+	public void removerTipoPatrimonio(int id) {
+		tipo_patrimonioDAO.removerTipoPatrimonio(id);
+	}
+	
+	public TipoPatrimonio atualizarTipoPatrimonio( int id, String novoNome_tipo_patrimonio) {
+		if(novoNome_tipo_patrimonio.length()<=45) {
+			TipoPatrimonio tipo_patrimonio = tipo_patrimonioDAO.atualizarTipoPatrimonio(id, novoNome_tipo_patrimonio);
+			return tipo_patrimonio;
+		}
+		return null;
+	}
+	
+	public TipoPatrimonio atualizarNome_tipo_patrimonioTipoPatrimonio( int id, String novoNome_tipo_patrimonio) {
+		if(novoNome_tipo_patrimonio.length()<=45) {
+			TipoPatrimonio tipo_patrimonio = tipo_patrimonioDAO.atualizarNome_tipo_patrimonioTipoPatrimonio(id, novoNome_tipo_patrimonio);
+			return tipo_patrimonio;
+		}
+		return null;
+	}
+	
+	
+	public List<TipoPatrimonio> listarTipoPatrimonio() {
+    	List<TipoPatrimonio> listaTipoPatrimonio= new ArrayList<>();
+    	TipoPatrimonioDAO tipo_patrimonio = new TipoPatrimonioDAO();
+    	listaTipoPatrimonio = tipo_patrimonio.listarTipoPatrimonio();
+    	
+    	for(TipoPatrimonio tp: listaTipoPatrimonio) {
+    		System.out.println(tp.getId()+"|"+tp.getNome_tipo_patrimonio());
+    	}
+    	return listaTipoPatrimonio;
+	}
+}
