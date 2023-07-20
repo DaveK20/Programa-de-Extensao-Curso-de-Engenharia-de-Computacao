9dde358ea49dd6460cbad3f61baee575b82642fc
Alinevguarisi <140089134+Alinevguarisi@users.noreply.github.com>
2023-07-20 16:14:48 -0300

    Update Cidade.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Cidade.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Cidade.java
@@ -3,11 +3,12 @@ package br.edu.iff.gestaopatrimonio.models;
 public class Cidade {
     private int id;
     private String nome;
+    private String sigla;
     
-    
-	public Cidade(int id, String nome) {
+	public Cidade(int id, String nome, String sigla) {
 		this.id = id;
 		this.nome = nome;
+		this.sigla = sigla;
 	}
 	public int getId() {
 		return id;
@@ -21,5 +22,13 @@ public class Cidade {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+    
+	
 
 }
