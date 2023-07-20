3dd512eeddcbffcc2d8c14b09ece3c46e1c7c036
Halisson José Soares da Rocha <hj_rocha@id.uff.br>
2023-07-20 12:21:52 -0300

    Commit Get e Set Estado

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Estado.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Estado.java
@@ -1,10 +1,34 @@
 package br.edu.iff.gestaopatrimonio.models;
 
 public class Estado {
-    private int id;
+	private int id;
 
-    private String sigla;
+	private String sigla;
 
-    private String nome;
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
