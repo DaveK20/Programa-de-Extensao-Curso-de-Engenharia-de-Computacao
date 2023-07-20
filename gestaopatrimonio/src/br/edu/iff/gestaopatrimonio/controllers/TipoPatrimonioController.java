4542960e42df456a5da484f2fdf093956aad5fd3
lucaspicancof <116851208+lucaspicancof@users.noreply.github.com>
2023-07-20 16:15:14 -0300

    Update TipoPatrimonioController.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/TipoPatrimonioController.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/TipoPatrimonioController.java
@@ -30,23 +30,14 @@ public class TipoPatrimonioController {
 		return null;
 	}
 	
-	public TipoPatrimonio atualizarNome_tipo_patrimonioTipoPatrimonio( int id, String novoNome_tipo_patrimonio) {
-		if(novoNome_tipo_patrimonio.length()<=45) {
-			TipoPatrimonio tipo_patrimonio = tipo_patrimonioDAO.atualizarNome_tipo_patrimonioTipoPatrimonio(id, novoNome_tipo_patrimonio);
-			return tipo_patrimonio;
-		}
-		return null;
-	}
-	
-	
 	public List<TipoPatrimonio> listarTipoPatrimonio() {
-    	List<TipoPatrimonio> listaTipoPatrimonio= new ArrayList<>();
-    	TipoPatrimonioDAO tipo_patrimonio = new TipoPatrimonioDAO();
-    	listaTipoPatrimonio = tipo_patrimonio.listarTipoPatrimonio();
-    	
-    	for(TipoPatrimonio tp: listaTipoPatrimonio) {
-    		System.out.println(tp.getId()+"|"+tp.getNome_tipo_patrimonio());
-    	}
-    	return listaTipoPatrimonio;
+	    	List<TipoPatrimonio> listaTipoPatrimonio= new ArrayList<>();
+	    	TipoPatrimonioDAO tipo_patrimonio = new TipoPatrimonioDAO();
+	    	listaTipoPatrimonio = tipo_patrimonio.listarTipoPatrimonio();
+	    	
+	    	for(TipoPatrimonio tp: listaTipoPatrimonio) {
+	    		System.out.println(tp.getId()+"|"+tp.getNome_tipo_patrimonio());
+	    	}
+	    	return listaTipoPatrimonio;
 	}
 }
