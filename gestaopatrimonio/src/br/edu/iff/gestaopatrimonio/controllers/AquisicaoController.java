6e6170e86b1644bd35a1843fac75de7b724b3b00
Ayrton Borges <42078126+AyrtonBorges@users.noreply.github.com>
2023-07-20 13:55:07 -0300

    Update AquisicaoController.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/AquisicaoController.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/controllers/AquisicaoController.java
@@ -12,7 +12,6 @@ public class AquisicaoController {
         aquisicaoDAO = new AquisicaoDAO();
     }
 
-    // Inserir uma nova aquisição
     public void inserirAquisicao(Aquisicao aquisicao, int patrimonio_id) {
         if(aquisicaoDAO.insertAquisicao(aquisicao, patrimonio_id))
         	System.out.println("Inserido com sucesso!");
@@ -20,12 +19,10 @@ public class AquisicaoController {
         	System.out.println("Ocorreu erro ao inserir!");
     }
 
-    // Obter todas as aquisições
     public List<Aquisicao> obterTodasAquisicoes() {
         return aquisicaoDAO.getAllAquisicoes();
     }
 
-    // Atualizar uma aquisição existente
     public void atualizarAquisicao(Aquisicao aquisicao) {
         if(aquisicaoDAO.updateAquisicao(aquisicao))
         	System.out.println("Atualizado com sucesso!");
@@ -33,21 +30,18 @@ public class AquisicaoController {
         	System.out.println("Ocorreu erro ao atualizar!");
     }
 
-    // Excluir uma aquisição
     public void excluirAquisicao(int id) {
         if(aquisicaoDAO.deleteAquisicao(id))
         	System.out.println("Excluído com sucesso!");
         else
         	System.out.println("Ocorreu erro ao excluir!");
     }
-    
-    // Obter todas Aquisições por ID do Patrimônio
+
     public List<Aquisicao> obterTodasAquisicoesporPatrimonioID(int patrimonio_id)
     {
     	return aquisicaoDAO.getAllAquisicaoByIdPatrimonio(patrimonio_id);
     }
-    
-    // Obter Aquisição por ID
+
     public Aquisicao obterAquisicaoporID(int id)
     {
     	return aquisicaoDAO.getAquisicaoById(id);
