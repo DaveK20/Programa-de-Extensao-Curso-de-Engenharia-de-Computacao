c3a8a20ee10679862332966d28f0df8f2ac78441
lucaspicancof <116851208+lucaspicancof@users.noreply.github.com>
2023-07-20 14:51:58 -0300

    Update Material.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Material.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Material.java
@@ -3,5 +3,25 @@ package br.edu.iff.gestaopatrimonio.models;
 public class Material {
     private int id;
     private String nome;
+    
+    
+    public Material (int id, String nome) {
+    	setId(id);
+    	setNome(nome);
+    }
+    
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
