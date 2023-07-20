feb496147338b055192255da22392238e20c0331
DaveK20 <42078238+DaveK20@users.noreply.github.com>
2023-07-20 17:07:25 -0300

    Update Imagem.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Imagem.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/Imagem.java
@@ -1,15 +1,22 @@
 package br.edu.iff.gestaopatrimonio.models;
 
 public class Imagem {
-	private int id;
+    private int id;
 
-	private String nome;
+    private String nome;
 
-	private String data;
+    private String data;
 
-	private String arquivo;
+    private String arquivo;
 
-	private TipoImagem tipoImagem;
+    private TipoImagem tipoImagem;
+    
+    public Imagem(String nome, String data, String arquivo, TipoImagem tipoImagem) {
+    	setNome(nome);
+    	setData(data);
+    	setArquivo(arquivo);
+    	setTipoImagem(tipoImagem);
+    }
 
 	public int getId() {
 		return id;
