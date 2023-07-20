22df330f7153f9d657096e31ab388d5b8599886e
gabientt <140023649+gabientt@users.noreply.github.com>
2023-07-20 15:33:20 -0300

    PalavraChave.java

--- gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/PalavraChave.java
+++ gestaopatrimonio/src/br/edu/iff/gestaopatrimonio/models/PalavraChave.java
@@ -1,25 +1,53 @@
 package br.edu.iff.gestaopatrimonio.models;
-
-public class PalavraChave {
-	private int id;
-
-	private String palavra;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getPalavra() {
-		return palavra;
-	}
-
-	public void setPalavra(String palavra) {
-		this.palavra = palavra;
-	}
-
 	
-}
+
+	public class PalavraChave {
+		private int id;
+		private String palavra;
+		private int patrimonio_id;
+		
+		public PalavraChave(){
+			
+		}
+
+		public PalavraChave(int id, String palavra, int patrimonio_id) {
+			this.id = id;
+			this.palavra = palavra;
+			this.patrimonio_id = patrimonio_id;
+		}
+
+
+
+		public int getId() {
+			return id;
+		}
+
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+
+		public String getPalavra() {
+			return palavra;
+		}
+
+
+		public void setPalavra(String palavra) {
+			this.palavra = palavra;
+		}
+
+
+		public int getPatrimonio_id() {
+			return patrimonio_id;
+		}
+
+
+		public void setPatrimonio_id(int patrimonio_id) {
+			this.patrimonio_id = patrimonio_id;
+		}
+
+
+
+	}
+
